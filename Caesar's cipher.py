9ce646d54aa31016ffca0389f3c15aa8aa24139d
Dmitry <homa88.01@mail.ru>
2021-01-06 15:58:35 +0500

    Добавление log

--- Caesar's cipher.py
+++ Caesar's cipher.py
@@ -1,6 +1,10 @@
+import logging
+
 if __name__ == '__main__':
     A = "abcdefghijklmnopqrstuvwxyz"
     B = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+    logging.basicConfig(filename="sample.log", level=logging.INFO)
+    logging.info("Начало работы")
     text = ''
     new_text = ''
     exit = False
@@ -9,17 +13,23 @@ if __name__ == '__main__':
         print("1) Зашифровать текст")
         print("2) Расшифровать текст")
         print("3) Выход")
+        logging.info("Выберите действие: 1) Зашифровать текст 2) Расшифровать текст 3) Выход")
         value = int(input())
         if value == 1: # Зашифровать тест
+            logging.info("Зашифровка текста")
             new_text = ''
             text = str(input("Введите текст который надо зашифровать: "))
+            logging.info("Введите текст который надо зашифровать: ")
+            p = "Введенный тест: " + text
+            logging.info(p)
             text = text.lower() # преобразование всех букв в строчные
+            logging.info("Преобразование всех букв в строчные...")
+            logging.info("Изменение букв и добавление их к строке с зашифрованном текстом...")
             for i in range(len(text)):
                 if A.find(text[i]) != -1: # проверка на английские буквы
                     number = A.find(text[i]) # нахождение цифры, под которой находится данная введенная буква в алфавите в начале программы
                     if number <= 22:
-                        number = A[A.find(text[i]) + 3] # нахождение цифры, под которой находится преобразованная буква в алфавите в начале программы
-                        new_text = new_text + A[number] # Добавление измененной буквы к строке с зашифрованном тексте
+                        new_text = new_text + A[A.find(text[i]) + 3] # Добавление измененной буквы к строке с зашифрованном тексте
                     elif number == 23:
                         new_text = new_text + 'a' # Добавление буквы 'a' к строке с зашифрованном тексте
                     elif number == 24:
@@ -29,8 +39,7 @@ if __name__ == '__main__':
                 elif B.find(text[i]) != -1: # проверка на русские буквы
                     number = B.find(text[i]) # нахождение цифры, под которой находится данная введенная буква в алфавите в начале программы
                     if number <= 29:
-                        number = B[B.find(text[i]) + 3] # нахождение цифры, под которой находится преобразованная буква в алфавите в начале программы
-                        new_text = new_text + B[number] # Добавление измененной буквы к строке с зашифрованном тексте
+                        new_text = new_text + B[B.find(text[i]) + 3] # Добавление измененной буквы к строке с зашифрованном тексте
                     elif number == 30:
                         new_text = new_text + 'а' # Добавление буквы 'a' к строке с зашифрованном тексте
                     elif number == 31:
@@ -43,10 +52,20 @@ if __name__ == '__main__':
             print("Исходный текст: ", text)
             print("Зашифрованный текст: ", new_text)
             print()
-        elif value == 2:
+            text = str("Исходный текст: " + text)
+            new_text = str("Зашифрованный текст: " + new_text)
+            logging.info(text)
+            logging.info(new_text)
+        elif value == 2: # Расшифровать тест
+            logging.info("Расшифровка текста")
             new_text = ''
             text = str(input("Введите текст который надо расшифровать: "))
+            logging.info("Введите текст который надо зашифровать: ")
+            p = "Введенный тест: " + text
+            logging.info(p)
             text = text.lower() # преобразование всех букв в строчные
+            logging.info("Преобразование всех букв в строчные...")
+            logging.info("Изменение букв и добавление их к строке с зашифрованном текстом...")
             for i in range(len(text)):
                 if A.find(text[i]) != -1: # проверка на английские буквы
                     number = A.find(text[i])  # нахождение цифры, под которой находится данная введенная буква в алфавите в начале программы
@@ -74,8 +93,15 @@ if __name__ == '__main__':
             print("Исходный текст: ", text)
             print("Расшифрованный текст: ", new_text)
             print()
+            text = str("Исходный текст: " + text)
+            new_text = str("Расшифрованный текст: " + new_text)
+            logging.info(text)
+            logging.info(new_text)
         elif value == 3:
             exit = True
             print("Выход...")
+            logging.info("Выход...")
+            logging.info("Process finished with exit code 0")
         else:
             print("Неверный формат ввода, попробуйте еще раз.")
+            logging.error("Неверный формат ввода")
